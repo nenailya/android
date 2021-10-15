a51cc009fe393ddfb2fee071f6c0a3bda43e55cf
n_zgr <nzgr1@mail.ru>
2021-10-15 15:12:36 +0300

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        log("onCreate")
         setContentView(R.layout.activity_main)
     }
+    override fun onStart() {
+        super.onStart()
+        log("onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        log("onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        log("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    val tag = "LIFECYCLE"
+
+    private fun log(msg: String) {
+        Log.d(tag, msg)
+    }
+
+
 }
